fbd68c68c984233d32c73c262416c5eb4601a982
Ajeet Mishra <mahi.aayush@gmail.com>
2021-02-09 11:34:32 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  title = "ng-multiselect-dropdown";
+  countries: Array<any> = [];
+  selCountries = [
+    {
+      item_id: 1,
+      item_text: "India",
+      image: "http://www.sciencekids.co.nz/images/pictures/flags96/India.jpg"
+    },
+    {
+      item_id: 5,
+      item_text: "Israel",
+      image: "http://www.sciencekids.co.nz/images/pictures/flags96/Israel.jpg"
+    }
+  ];
+  dropdownSettings: any = {};
+
+  ngOnInit() {
+    this.countries = [
+      {
+        item_id: 1,
+        item_text: "India",
+        image: "http://www.sciencekids.co.nz/images/pictures/flags96/India.jpg"
+      },
+      {
+        item_id: 2,
+        item_text: "Spain",
+        image: "http://www.sciencekids.co.nz/images/pictures/flags96/Spain.jpg"
+      },
+      {
+        item_id: 3,
+        item_text: "United Kingdom",
+        image:
+          "http://www.sciencekids.co.nz/images/pictures/flags96/United_Kingdom.jpg"
+      },
+      {
+        item_id: 4,
+        item_text: "Canada",
+        image:
+          "http://www.sciencekids.co.nz/images/pictures/flags96/Canada.jpg",
+        isDisabled: true
+      },
+      {
+        item_id: 5,
+        item_text: "Israel",
+        image: "http://www.sciencekids.co.nz/images/pictures/flags96/Israel.jpg"
+      },
+      {
+        item_id: 6,
+        item_text: "Brazil",
+        image: "http://www.sciencekids.co.nz/images/pictures/flags96/Brazil.jpg"
+      },
+      {
+        item_id: 7,
+        item_text: "Barbados",
+        image:
+          "http://www.sciencekids.co.nz/images/pictures/flags96/Barbados.jpg"
+      },
+      {
+        item_id: 8,
+        item_text: "Mexico",
+        image: "http://www.sciencekids.co.nz/images/pictures/flags96/Mexico.jpg"
+      }
+    ];
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: "item_id",
+      textField: "item_text",
+      selectAllText: "Select All",
+      unSelectAllText: "UnSelect All",
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+  }
+
+  get getItems() {
+    return this.countries.reduce((acc, curr) => {
+      acc[curr.item_id] = curr;
+      return acc;
+    }, {});
+  }
+
+  onItemSelect(item: any) {
+    console.log("onItemSelect", item);
+  }
+  onSelectAll(items: any) {
+    console.log("onSelectAll", items);
+  }
+}
